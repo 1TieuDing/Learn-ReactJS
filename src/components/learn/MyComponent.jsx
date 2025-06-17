289c37c6059dd4cfef25958142de44c917587e10
1TieuDing <vtuananhh@gmail.com>
2025-06-17 14:18:20 +0700

    video 33 + data type

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,15 @@
 import './style.css'
 
 const MyComponent = () => {
+    const TieuDing = {
+        name: "1TieuDing",
+        age: 23,
+    }
+
     return (
         <>
-            <div>ITieuDing!!!</div>
+            <div>{JSON.stringify(TieuDing)} & ITieuDing!!!</div>
+            <div>{console.log("TieuDing")}</div>
             <div className="Child" style={{ fontSize: "12px" }}>child</div>
         </>
     )
