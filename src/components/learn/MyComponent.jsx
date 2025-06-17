8ae6dee08faf1949bf24861182ed36f36702a037
1TieuDing <vtuananhh@gmail.com>
2025-06-17 14:00:42 +0700

    video 30 + JSX, Fragment

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,13 @@
+
+import './style.css'
+
+const MyComponent = () => {
+    return (
+        <>
+            <div>ITieuDing!!!</div>
+            <div className="Child" style={{ fontSize: "12px" }}>child</div>
+        </>
+    )
+}
+
+export default MyComponent
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,17 @@
+
+const SecondComponent = () => {
+    return (
+        <div>ITieuDing123</div>
+    )
+}
+
+const ThirdComponent = () => {
+    return (
+        <div>ITieuDing</div>
+    )
+}
+
+export {
+    SecondComponent,
+    ThirdComponent
+}
