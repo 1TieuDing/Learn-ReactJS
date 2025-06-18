37cd917aa1be977a2ad0476ba90e721caf5019ef
1TieuDing <vtuananhh@gmail.com>
2025-06-18 15:36:44 +0700

    video 73 + useEffect

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,13 +1,18 @@
 import { Space, Table, Tag } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchUserApi } from '../../services/api.service';
 
 
 const UserTable = () => {
-    const [dataSource, setDataUser] = useState([
+    const [dataUsers, setDataUsers] = useState([
         { _id: 1, fullName: "TieuDing", email: "tieuding@gmail.com" },
         { _id: 2, fullName: "TieuDing1", email: "tieuding1@gmail.com" }
     ])
+
+    useEffect(() => {
+        console.log(">>> run effect 111")
+        loadUser()
+    }, [])
 
     const columns = [
         {
@@ -26,13 +31,18 @@ const UserTable = () => {
 
     const loadUser = async () => {
         const res = await fetchUserApi()
+        setDataUsers(res.data)
     }
 
-    loadUser()
-    console.log(">>> run render")
+
+    console.log(">>> run render 000")
 
     return (
-        <Table columns={columns} dataSource={dataSource} />
+        <Table
+            columns={columns}
+            dataSource={dataUsers}
+            rowKey={"_id"}
+        />
     )
 }
 
