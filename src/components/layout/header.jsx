fb118f647f43b95982eae6e2700a25e4ea3914d5
1TieuDing <vtuananhh@gmail.com>
2025-06-19 19:55:59 +0700

    video 98 + react context api

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom'
 import { Menu } from 'antd'
 import { UsergroupAddOutlined, HomeOutlined, AuditOutlined, SettingOutlined } from '@ant-design/icons';
-import { Children, useState } from 'react';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../context/auth.context';
 
 const Header = () => {
     const [current, setCurrent] = useState('');
+
+    const { user } = useContext(AuthContext)
+
+    console.log(">>> check user: ", user)
+
     const onClick = e => {
         console.log('click ', e);
         setCurrent(e.key);
